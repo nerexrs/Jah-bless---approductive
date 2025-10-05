33e7b9acf3b03e363b8037f40b81d8fc3afe43c7
samu-shaper <samu@shaper.us>
2025-10-05 18:27:02 -0500

    ni idea xd q se subio xd x2

--- src/app/modulos/clock/clock.component.ts
+++ src/app/modulos/clock/clock.component.ts
@@ -1,63 +1,11 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subscription, timer } from 'rxjs';
-import { SessionService } from '../../services/session.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-clock',
+  standalone: false,
   templateUrl: './clock.component.html',
-  styleUrls: ['./clock.component.scss']
+  styleUrl: './clock.component.scss'
 })
-export class ClockComponent implements OnDestroy {
-  minutes: number = 25;
-  seconds: number = 0;
-  timerSubscription: Subscription | undefined;
-  isPaused: boolean = false;
+export class ClockComponent {
 
-  constructor(private sessionService: SessionService) {}
-
-  startTimer() {
-    if (this.timerSubscription) {
-      this.timerSubscription.unsubscribe();
-    }
-
-    this.isPaused = false;
-    const totalSeconds = this.minutes * 60 + this.seconds;
-
-    this.timerSubscription = timer(0, 1000).subscribe(t => {
-      const remainingSeconds = totalSeconds - t;
-      if (remainingSeconds >= 0 && !this.isPaused) {
-        this.minutes = Math.floor(remainingSeconds / 60);
-        this.seconds = remainingSeconds % 60;
-      } else if (remainingSeconds < 0) {
-        this.finishSession();
-      }
-    });
-  }
-
-  pauseTimer() {
-    this.isPaused = true;
-  }
-
-  resetTimer() {
-    if (this.timerSubscription) {
-      this.timerSubscription.unsubscribe();
-    }
-    this.minutes = 25;
-    this.seconds = 0;
-    this.isPaused = false;
-  }
-
-  finishSession() {
-    if (this.timerSubscription) {
-      this.timerSubscription.unsubscribe();
-    }
-    this.sessionService.addSession({ type: 'Pomodoro', duration: 25, date: new Date() });
-    this.resetTimer();
-  }
-
-  ngOnDestroy() {
-    if (this.timerSubscription) {
-      this.timerSubscription.unsubscribe();
-    }
-  }
 }
